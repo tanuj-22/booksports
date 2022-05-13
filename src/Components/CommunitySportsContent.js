a2ba96540f6170fe1d3ff2568194e5ab00c3c7ff
tanuj-22 <56783833+tanuj-22@users.noreply.github.com>
2022-05-14 00:37:42 +0530

    Home page Content Modified

---
+++ src/Components/CommunitySportsContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../css/CommunitySportsContent.css";
+const CommunitySportsContent = () => {
+  return (
+    <div className="Comm-container">
+        Community Sports
+    </div>
+  )
+}
+
+export default CommunitySportsContent
---
+++ src/Components/HomePageMainContent.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
+import lightningLogo from "../assests/lightningLogo.svg";
+import MobileMockupLeft from "../assests/MobileMockupLeft.svg";
+import MobileMockupRight from "../assests/MobileMockupRight.svg";
+import MobileMockupIconLeft from "../assests/MobileMockupIconLeft.svg";
+import MobileMockupIconRight from "../assests/MobileMockupIconRight.svg";
+
+import "../css/HomePageMainContent.css";
+const HomePageMainContent = () => {
+  const inputRef = useRef();
+  const [length, setlength] = useState(30);
+
+  useEffect(() => {
+    if (inputRef && inputRef.current && window.innerWidth >= 560) {
+      setlength(
+        ((window.innerHeight -
+          inputRef.current.getBoundingClientRect().y +
+          inputRef.current.getBoundingClientRect().height) *
+          100) /
+          window.innerHeight -
+          10
+      );
+    } else if (inputRef && inputRef.current && window.innerWidth < 560) {
+      setlength(
+        ((window.innerHeight -
+          inputRef.current.getBoundingClientRect().y +
+          inputRef.current.getBoundingClientRect().height) *
+          100) /
+          window.innerHeight -
+          6
+      );
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="HomeMainContent">
+        <div className="bigText">Play sport. Simplified.</div>
+
+        <div className="smallText">
+          Unified solution of participating in sport games and activities. Play
+          your favourite games anytime, anywhere with ease.
+        </div>
+        <Button className="JoinButton" id="join-waitlist-btn" variant="primary">
+          <span className="join-btn-text">Join Waitlist</span>
+          <img
+            src={lightningLogo}
+            alt="lightning-logo"
+            className="lightningLogo"
+          />
+        </Button>
+        <div ref={inputRef} className="mockup-background-container">
+          <div
+            style={{ height: length.toString() + "vh" }}
+            className="HomeMainContentBackground"
+          >
+            &nbsp;
+          </div>
+          <div className="mobile-mockup-container">
+            <img
+              src={MobileMockupLeft}
+              alt="mobile-mockup"
+              className="mobileMockup"
+            />
+            <img
+              src={MobileMockupRight}
+              alt="mobile-mockup"
+              className="mobileMockup-1"
+            />
+          </div>
+          <div className="mockup-icons">
+            <img
+              src={MobileMockupIconLeft}
+              className="mockup-icon-left"
+              alt="mobile-mockup"
+            />
+            <img
+              src={MobileMockupIconRight}
+              className="mockup-icon-right"
+              alt="mobile-mockup"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePageMainContent;
