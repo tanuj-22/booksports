c8cdacbc809ef2735778fe53f99d84155c73c34f
Karan Lakhwani <karan.lakhwani0910@gmail.com>
2022-05-16 18:29:18 +0530

    supporter cards added

--- src/Components/SupporterResponse.js
+++ src/Components/SupporterResponse.js
@@ -11,12 +11,14 @@ const SupporterResponse = (props) => {
                     <img src={props.img} alt='supporter'/>
                 </Col>
                 <Col xs={10}>
-                    <p className='description px-1'>
-                        {props.description}
-                    </p>
-                    <p className='intro'>
-                        {props.intro}
-                    </p>
+                    <div className='supporter-info'>
+                        <p className='description'>
+                            {props.description}
+                        </p>
+                        <p className='intro'>
+                            {props.intro}
+                        </p>
+                    </div>
                 </Col>
             </Row>
         </Container>
