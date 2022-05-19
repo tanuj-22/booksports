15bbdd62bfa92865e16809954ebbed37a652695d
tanuj-22 <56783833+tanuj-22@users.noreply.github.com>
2022-05-19 12:15:21 +0530

    com-sports-content completed

--- src/Components/CommunitySportsContent.js
+++ src/Components/CommunitySportsContent.js
@@ -2,14 +2,32 @@ import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import lightningLogo from "../assests/lightningLogo.svg";
 import "../css/CommunitySportsContent.css";
+import img1 from "../assests/masonryGrid/33.png";
+import img2 from "../assests/masonryGrid/34.png";
+import img3 from "../assests/masonryGrid/35.png";
+import img4 from "../assests/masonryGrid/36.png";
+import img5 from "../assests/masonryGrid/37.png";
+import img6 from "../assests/masonryGrid/39.png";
+import img7 from "../assests/masonryGrid/41.png";
+import img8 from "../assests/masonryGrid/comm-sport-mobile.png";
 const CommunitySportsContent = () => {
   return (
     <div className="Comm-container">
-      <Container>
-        <Row>
-          <Col md={{ span: 6, order: 2 }}>Pics</Col>
-          <Col md={{ span: 6, order: 1 }}>
-            <Container>
+      <Container className="block-container">
+        <Row className="align-items-start">
+          <Col className="column-container " md={{ span: 6, order: 2 }}>
+            <div className="img-container" id="left-container-img">
+              {[img1, img3, img5, img2, img4, img6, img7].map((img, index) => {
+                return <img src={img} key={index} alt={"img" + index} />;
+              })}
+            </div>
+            
+            <div className="mobile-img-container">
+              <img src={img8} alt="img1" />
+            </div>
+          </Col>
+          <Col className="column-container" md={{ span: 6, order: 1 }}>
+            <Container fluid className="com-sports-content-container">
               <div className="com-sports-content">
                 <h2>The power of community sports.</h2>
                 <p>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,20 +4,16 @@ import HomePageMainContent from "../Components/HomePageMainContent";
 
 import NavbarHome from "../Components/NavbarHome";
 
-
-
 const Home = () => {
-  
-
-
   return (
-    <div className="HomePageContent">
-      <div className="landingMainContent">&nbsp;</div>
-      <NavbarHome />
-      <HomePageMainContent/>
-      <CommunitySportsContent/>
-      
-    </div>
+    <>
+      <div className="HomePageContent">
+        <div className="landingMainContent">&nbsp;</div>
+        <NavbarHome />
+        <HomePageMainContent />
+      </div>
+      <CommunitySportsContent />
+    </>
   );
 };
 
