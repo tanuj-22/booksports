1061a9291e51b5a092d26c046398e5c6baf54953
tanuj-22 <56783833+tanuj-22@users.noreply.github.com>
2022-05-21 00:21:17 +0530

    ids for links added

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -2,42 +2,37 @@
 import PricingTable from "./PricingTable";
 import { Container, Row, Col } from "react-bootstrap";
 // import css
-import '../css/pricing.css';
+import "../css/pricing.css";
 // import assets
-import savemoney from '../assests/savemoney.svg';
+import savemoney from "../assests/savemoney.svg";
 
 const Pricing = () => {
   return (
-    <div className="pricing-section">
-        <Container>
-            <p className="pricing-tagline">
-                A plan for everyone
-            </p>
-            <p className="pricing-heading">
-                Pricing
-            </p>
-            <div className="tab-section">
-                <Container fluid>
-                    <Row>
-                        <Col xs={6}>
-                            <p className="tab-section-yearly">Yearly</p>
-                        </Col>
-                        <Col xs={6}>
-                            <p className="tab-section-monthly">Monthly</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+    <div className="pricing-section" id="pricing">
+      <Container>
+        <p className="pricing-tagline">A plan for everyone</p>
+        <p className="pricing-heading">Pricing</p>
+        <div className="tab-section">
+          <Container fluid>
+            <Row>
+              <Col xs={6}>
+                <p className="tab-section-yearly">Yearly</p>
+              </Col>
+              <Col xs={6}>
+                <p className="tab-section-monthly">Monthly</p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
 
-            {/* vector */}
-            <div className="save-money">
-                <img src={savemoney} alt=""/>
-            </div>
-            <PricingTable/>
-
-        </Container>
+        {/* vector */}
+        <div className="save-money">
+          <img src={savemoney} alt="" />
+        </div>
+        <PricingTable />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
