8771d84ca61804967897845c335aadbc1841f0c2
Karan Lakhwani <karan.lakhwani0910@gmail.com>
2022-05-20 12:29:51 +0530

    pricing table for desktop view added

--- src/Components/PricingTable.js
+++ src/Components/PricingTable.js
@@ -1,7 +1,23 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+// import bootstrap
+import { Table, Button } from 'react-bootstrap'
+// import assets
+import correct from '../assests/correct.svg';
+import lightningLogo from '../assests/lightningLogo.svg';
+import lightningLogoBlack from '../assests/lightninglogo-black.svg'
+// import libs
+import { useState } from 'react';
 
 const PricingTable = () => {
+
+    const [logo, setLogo] = useState(lightningLogoBlack);
+
+    const hoverOnButton = () => {
+        setLogo(lightningLogo);
+    }
+    const hoverOffButton = () => {
+        setLogo(lightningLogoBlack);
+    }
+
   return (
     <div className='pricing-table'>
         <Table responsive>
@@ -15,7 +31,112 @@ const PricingTable = () => {
             </thead>
             <tbody>
                 <tr>
-                
+                    <td>Pricing</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Features</th>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Organise Games and Events</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Create Groups and Messenger</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Reserve your spots</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Smart Waiting List</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Split booking fees evenly</td>
+                    <td>-</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Advanced Organiser settings</td>
+                    <td>-</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Receive online payments</td>
+                    <td>-</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Choose Refund deadline</td>
+                    <td>-</td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Recurring events / games
+                    </td>
+                    <td>
+                        -
+                    </td>
+                    <td>
+                        <img src={correct} alt=""/>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <Button onMouseOver={hoverOnButton} onMouseOut={hoverOffButton} className="join-button-pricing-outline" id="join-waitlist-btn" variant="outline-dark">
+                            <span className="join-btn-text">Join Waitlist</span>
+                            <img
+                                src={logo}
+                                alt="lightning-logo"
+                                className="lightningLogo"
+                            />
+                        </Button>
+                    </td>
+                    <td>
+                        <Button className="join-button-pricing" id="join-waitlist-btn" variant="dark">
+                            <span className="join-btn-text">Join Waitlist</span>
+                            <img
+                                src={lightningLogo}
+                                alt="lightning-logo"
+                                className="lightningLogo"
+                            />
+                        </Button>
+                    </td>
                 </tr>
             </tbody>
         </Table>
