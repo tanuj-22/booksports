7c09e6e828acbbd9fc7a133d3a5ad696354bd8a7
Karan Lakhwani <karan.lakhwani0910@gmail.com>
2022-05-20 09:31:52 +0530

    pricing updated

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,22 +1,29 @@
-import React from 'react'
+// import components
+import PricingTable from "./PricingTable";
+import { Container } from "react-bootstrap";
+// import css
+import '../css/pricing.css';
 
 const Pricing = () => {
   return (
-    <div>
-        <p>
-            A plan for everyone
-        </p>
-        <p>
-            Pricing
-        </p>
-        <p>
-            <span>
-                Yearly
-            </span>
-            <span>
-                Monthly
-            </span>
-        </p>
+    <div className="pricing-section">
+        <Container>
+            <p className="pricing-tagline">
+                A plan for everyone
+            </p>
+            <p className="pricing-heading">
+                Pricing
+            </p>
+            <p>
+                <span>
+                    Yearly
+                </span>
+                <span>
+                    Monthly
+                </span>
+            </p>
+            <PricingTable/>
+        </Container>
     </div>
   )
 }
---
+++ src/Components/PricingTable.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+
+const PricingTable = () => {
+  return (
+    <div className='pricing-table'>
+        <Table responsive>
+            <thead>
+                <tr>
+                    <td>Plans</td>
+                    <th>Users</th>
+                    <th>Pro</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                
+                </tr>
+            </tbody>
+        </Table>
+    </div>
+  )
+}
+
+export default PricingTable
